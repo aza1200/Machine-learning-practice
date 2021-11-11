d3b80af7a0958b040bd6727c8bd42c498a44ee8b
2018008068 <aza1200@naver.com>
2021-11-12 02:03:29 +0900

    start decision tree

---
+++ Decision tree/01_start_setting.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import data
+
+train_df = pd.DataFrame(data.district_dict_list)
+train_df = train_df[['district','longitude','latitude','label']]
+
+test_df = pd.DataFrame(data.dong_dict_list)
+test_df = test_df[['dong','longitude','latitude','label']]
+
+print(train_df.label.value_counts())
+print(test_df.label.value_counts())
+
+#print(train_df.head())
+print(train_df.columns)
+print(train_df.index)
+#print(train_df.values)
